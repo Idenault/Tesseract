853fe18aaf96839885cc1e56527c7a59dd616d38
Snoopalukas <33130026+Snoopalukas@users.noreply.github.com>
2017-10-26 17:51:11 -0400

    Create Playlist.cpp

---
+++ Playlist.cpp
@@ -0,0 +1,50 @@
+
+#include "Playlist.h"
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+Playlist::Playlist(string playlistName, int playlistID) {
+    name = playlistName;
+    id = playlistID;
+}
+
+Playlist::~Playlist() {
+    cout << "Destroying Playlist. Name: " << name << " ID: " << id;
+}
+Playlist::Playlist(Playlist &copy) {
+    cout << "Copy constructor made.";
+}
+
+string Playlist::getName() {
+    return name;
+}
+
+int Playlist::getID() {
+    return id;
+}
+
+vector<Track*> Playlist::getTracksInPlaylist(){
+    return tracksInPlaylist;
+}
+
+void Playlist::addTrackToPlaylist(Track track) {
+    tracksInPlaylist.push_back(&track);
+}
+
+void Playlist::removeTrackFromPlaylist(Track track) {
+    for (int i=0; i < tracksInPlaylist.size(); i++){
+        if (tracksInPlaylist[i] == &track){
+            tracksInPlaylist.erase(tracksInPlaylist.begin()+i);
+        }
+    }
+}
+
+string Playlist::toString() {
+    cout << "Displaying playlist: " << name << " ID: " << id << "\n";
+    for (int i=0; i < tracksInPlaylist.size(); i++){
+        cout << (*tracksInPlaylist[i])->toString() << "\n";
+    }
+}

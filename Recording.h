db6653bbc4fb86b45178416edbb099411c077ccf
Snoopalukas <33130026+Snoopalukas@users.noreply.github.com>
2017-10-28 18:58:13 -0400

    part of linking update

--- Recording.h
+++ Recording.h
@@ -1,10 +1,10 @@
-
-
 #ifndef TESSERACT_TEST_RECORDING_H
 #define TESSERACT_TEST_RECORDING_H
 
 #include <string>
 #include "str_util.h"
+#include "Track.h"
+#include <vector>
 
 using namespace std;
 
@@ -13,20 +13,23 @@ class Recording
 
 public:
 
-    Recording(const string t, const string a, const string p, int y, int aID);
-    Recording(Recording& copy);
-    ~Recording();
-    int getAlbumID()const;
-    string toString()const;
+	Recording(const string t, const string a, const string p, int y, int aID);
+	Recording(Recording& copy);
+	~Recording();
+	int getAlbumID()const;
+	string toString()const;
+	vector<Track*> getTracks();
+	void addTrack(Track* trackPointer);
 
 private:
 
-    string title;
-    string artist;
-    string producer;
-    int year;
-    int albumID;
-    StrUtil formater;
+	string title;
+	string artist;
+	string producer;
+	int year;
+	int albumID;
+	StrUtil formater;
+	vector<Track*> tracks;
 };
 ostream& operator<<(ostream& out, const Recording& recordingObject);
 

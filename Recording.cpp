c523b4eedc26a1ebf72a6f248d33c63cb01447a9
Snoopalukas <33130026+Snoopalukas@users.noreply.github.com>
2017-10-29 17:07:52 -0400

    Update Recording.cpp

--- Recording.cpp
+++ Recording.cpp
@@ -3,56 +3,56 @@
 
 Recording::Recording(const string t, const string a, const string p, int y, int aID)
 {
-    //cout << "BUILDING A RECORDING!!!!"<<endl;
-    title =t;
-    artist =a;
-    producer = p;
-    year = y;
-    albumID = aID;
+	cout << "BUILDING A RECORDING!!!!"<<endl;
+	title =t;
+	artist =a;
+	producer = p;
+	year = y;
+	albumID = aID;
 }
 
 Recording::Recording(Recording &copy){cout << "this Should not happen"<<endl;}
 
 Recording::~Recording()
 {
-    //cout << "DESTROYING A RECORDING!!!!"<<endl;
+	cout << "DESTROYING A RECORDING!!!!"<<endl;
 }
 
 string Recording::toString() const
 {
-    return "ALBUM TITLE: " + formater.toTitleCase(title) + " ARTIST: " + formater.toTitleCase(artist) + " PRODUCER: " + producer + " YEAR: " + to_string(year) + " ID: " + to_string(albumID);
+	return "ALBUM TITLE: " + formater.toTitleCase(title) + " ARTIST: " + formater.toTitleCase(artist) + " PRODUCER: " + producer + " YEAR: " + to_string(year) + " ID: " + to_string(albumID);
 }
 
 string Recording::toStringWithTracks() const
 {
-    string returnStr = "ALBUM TITLE: " + formater.toTitleCase(title) + " ARTIST: " + formater.toTitleCase(artist) + " PRODUCER: " + producer + " YEAR: " + to_string(year) + " ID: " + to_string(albumID) +"\n";
-    returnStr += "TRACKS: ";
-    returnStr += "\n";
+	string returnStr = "ALBUM TITLE: " + formater.toTitleCase(title) + " ARTIST: " + formater.toTitleCase(artist) + " PRODUCER: " + producer + " YEAR: " + to_string(year) + " ID: " + to_string(albumID) +"\n";
+	returnStr += "TRACKS: ";
+	returnStr += "\n";
 
-    if(!tracks.empty())
-    {
-        for(auto& t: tracks)
-        {
-            returnStr += t->toString();
-        }
-    }
-    else{returnStr += "this Recording is Empty";}
+	if(!tracks.empty())
+	{
+		for(auto& t: tracks)
+		{
+			returnStr += t->toString();
+		}
+	}
+	else{returnStr += "this Recording is Empty";}
 
-    return returnStr;
+	return returnStr;
 }
 
 int Recording::getAlbumID() const{
-    return albumID;
+	return albumID;
 }
 
 vector<Track*> Recording::getTracks() {
-    return tracks;
+	return tracks;
 }
 void Recording::addTrack(Track* trackPointer){
-    tracks.push_back(trackPointer);
+	tracks.push_back(trackPointer);
 }
 
 ostream& operator<<(ostream& out, const Recording& recordingObject)
 {
-    out<<recordingObject.toString()<<endl;
+	out<<recordingObject.toString()<<endl;
 }

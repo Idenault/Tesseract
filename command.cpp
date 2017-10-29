b4b328efe040256d774a96f99f90026920288c3b
Snoopalukas <33130026+Snoopalukas@users.noreply.github.com>
2017-10-29 01:44:53 -0400

    Update command.cpp

--- command.cpp
+++ command.cpp
@@ -1,3 +1,17 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * */
+/*                                                   */
+/*  Program:  MyTunes Music Player                   */
+/*  Author:   Louis Nel                              */
+/*  Date:     21-SEP-2017                            */
+/*                                                   */
+/*  (c) 2017 Louis Nel                               */
+/*  All rights reserved.  Distribution and           */
+/*  reposting, in part or in whole, requires         */
+/*  written consent of the author.                   */
+/*                                                   */
+/*  COMP 2404 students may reuse this content for    */ 
+/*  their course assignments without seeking consent */
+/* * * * * * * * * * * * * * * * * * * * * * * * * * */
 
 #include <string>
 #include <vector>
@@ -9,11 +23,9 @@ using namespace std;
 #include "command.h"
 
 
-
 Command::Command(string cmdStr){
-	Database db = *new Database;
 	commandString = cmdStr;
-	parseAndEditDB(commandString);
+	parseAndEditDB();
 }
 
 string Command::getCommandString(){return commandString;}
@@ -45,7 +57,13 @@ bool Command::isDevCommand(){
 		if(getCommandName().compare(DevCommands[i]) == 0) return true;
 	return false;
 }
-void Command::parseAndEditDB(string cmdStr) {
+
+vector<string> Command::getTokens() {
+	return tokens;
+}
+void Command::parseAndEditDB() {
+
+
 	/*
 	Parse (tokenize) the command 
 	
@@ -57,7 +75,7 @@ void Command::parseAndEditDB(string cmdStr) {
 	The Girl From Ipanema
 	*/
 	string buffer;
-	stringstream ss(cmdStr); // Insert the string into a stringstream
+	stringstream ss(commandString); // Insert the string into a stringstream
 
 	//Tokenize the command
 	//collect substrings between "" quotation marks as a single token
@@ -105,239 +123,4 @@ void Command::parseAndEditDB(string cmdStr) {
 
 	if (tokens.size() == 0) tokens.push_back("NO COMMAND");
 
-	//Command is now tokenized into tokens vector
-
-
-
-
-	//TOKENS VECTOR YO! 2EZ thanks Lou!
-
-	//shows use of (cheesing) iterator
-	for (vector<string>::iterator it = tokens.begin(); it != tokens.end(); ++it) {
-
-		//if adding something
-		if (*it == CMD_ADD) {
-			it++;
-
-			// if thing being added is a song to DB
-			if (*it == "-s" && tokens.size() == 5) {
-
-				int songID = stoi(*++it);
-				string title = *++it;
-				string composer = *++it;
-
-				db.createSong(title, composer, songID);
-
-				cout << "Adding Song: " << songID << " " << title << " " << composer << " to database.\n";
-				break;
-			}
-
-
-			//if thing being added is a recording
-			else if(*it == "-r" && tokens.size() == 7){
-
-				int albumID = stoi(*++it);
-				string title = *++it;
-				string artist = *++it;
-				string producer = *++it;
-				int year = stoi(*++it);
-
-				db.createRecording(title, artist, producer, year, albumID);
-
-				cout << "Adding Recording: " << albumID << " " << title << " " << artist << " " << producer << " " << year << " to database.\n";
-				break;
-			}
-
-
-			//if thing being added is a track
-			else if(*it == "-t" && tokens.size() == 6){
-
-				int albumID = stoi(*++it);
-				int songID = stoi(*++it);
-				int trackNum = stoi(*++it);
-
-				db.createTrack(songID, albumID, trackNum);
-				db.addTrackToRecording(albumID, songID);
-
-				cout << "Adding Track: " << songID << " " << trackNum << " to recording " << " " << albumID << "\n";
-				break;
-			}
-
-
-			//if thing being added is a user
-			else if(*it == "-u" && tokens.size() == 4){
-
-				string userID = *++it;
-				string userName = *++it;
-
-				db.creatUser(userID, userName);
-
-				cout << "Adding User: " << userID << " " << userName << " to database.\n";
-				break;
-			}
-
-
-			//if thing being added is a playlist
-			else if (*it == "-p" && tokens.size() == 5){
-
-				string userID = *++it;
-				string playlistName = *++it;
-				int playlistID = stoi(*++it);
-
-				db.addPlaylistToUser(userID, playlistName, playlistID);
-
-				cout << "Adding Playlist: " << playlistName << " " << playlistID << " to User: " << userID <<"\n";
-				break;
-			}
-
-
-			//if thing being added is a track to a playlist
-			else if (*it == "-l" && tokens.size() == 5){
-
-				string userID = *++it;
-				string playlistName = *++it;
-				int songID = stoi(*++it);
-
-				db.addTrackToPlaylist(songID, userID, playlistName);
-
-				cout << "Adding Song: " << songID << " " << " to User: " << userID << " Playlist: " << playlistName << "\n";
-				break;
-			}
-			else {
-				cout << "Unrecognized add command. Type .help for examples of proper syntax." << endl;
-				break;
-			}
-		}
-
-
-		//if removing something
-		if(*it == CMD_DELETE){
-			it++;
-
-			//if thing being removed is a song
-			if(*it == "-s" && tokens.size() == 3){
-				int songID = stoi(*++it);
-
-				db.removeSong(songID);
-
-				cout << "Removing Song: " << songID << endl;
-				break;
-
-			}
-
-			//if thing being removed is a recording
-			else if (*it == "-r" && tokens.size() == 3){
-				int albumID = stoi(*++it);
-
-				db.removeRecording(albumID);
-
-				cout << "Removing Recording: " << albumID << endl;
-				break;
-
-			}
-
-			//if thing being removed is a track from a recording
-			else if (*it == "-t" && tokens.size() == 4){
-				int songID = stoi(*++it);
-				int albumID = stoi(*++it);
-
-				db.removeTrackFromRecording(albumID, songID);
-
-				cout << "Removing track/song: " << songID << " from Recording: " << albumID << endl;
-				break;
-
-			}
-
-			//if thing being removed is user
-			else if (*it == "-u" && tokens.size() == 3){
-				string userID = *++it;
-
-				db.removeUserFromUsers(userID);
-
-				cout << "Removing User: " << userID << "\n";
-				break;
-			}
-
-			//if thing being removed is a playlist
-			else if (*it == "-p" && tokens.size() == 4){
-				string playlistName = *++it;
-				string userID = *++it;
-
-				db.removePlaylistFromUsers(userID, playlistName);
-
-				cout << "Removing Playlist: " << playlistName << " from User: " << userID << "\n";
-				break;
-			}
-
-			//if thing being removed is a track from a playlist
-			else if (*it == "-l" && tokens.size() == 5){
-				int songID = stoi(*++it);
-				string playlistName = *++it;
-				string userID = *++it;
-
-				db.removeTrackFromPlaylistByName(userID, playlistName, songID);
-
-				cout << "Removing Track: " << songID << " from User: " << userID << " from Playlist: " << playlistName << "\n";
-				break;
-			}
-
-			//else print "you're dumb"
-			else{
-				cout << "Unrecognized delete command. Type .help for examples of proper syntax." << endl;
-				break;
-			}
-
-		}
-
-
-		//if showing something
-		if(*it == CMD_SHOW){
-			it++;
-
-			//if thing being shown is all songs
-			if (*it == "songs" && tokens.size() == 2){
-
-				db.showSongs();
-				break;
-			}
-
-			//if thing being show is all users
-			else if (*it == "users" && tokens.size() == 2){
-				db.showUsers();
-				break;
-			}
-
-			//if thing being shown is all playlist belonging to a user
-			else if (*it == "playlists" && tokens.size() == 3){
-				string userID = *++it;
-				db.showUserPlaylists(userID);
-				break;
-			}
-
-			//if thing being shown is all tracks in a playlist
-			else if (*it == "songs" && tokens.size() == 4){
-				string userID = *++it;
-				string playlistName = *++it;
-
-				db.showSongsInPlaylist(userID, playlistName);
-				break;
-			}
-
-			//if thing being shown is all recordings
-			else if (*it == "recordings" && tokens.size() == 2){
-				db.showRecordingsWithoutTracks();
-				break;
-			}
-
-			//if thing being shown is all tracks
-			else if (*it == "tracks" && tokens.size() == 2){
-				db.showTracks();
-				break;
-			}
-
-			else{
-				cout << "Unrecognized show command. Type .help for examples of proper syntax." << endl;
-			}
-		}
-	}
 }
